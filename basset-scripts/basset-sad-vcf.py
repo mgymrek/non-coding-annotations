cdabf9d7587d8e10ab01c0358e0a97c842d6e7f6
mgymrek <mgymrek@mit.edu>
2016-01-08 22:20:18 +0000

    adding option to only run part of basset-sad-vcf

--- basset-scripts/basset-sad-vcf.py
+++ basset-scripts/basset-sad-vcf.py
@@ -12,6 +12,28 @@ sys.path = [os.path.join(os.environ["BASSETDIR"], "src")] + sys.path
 import vcf
 from util.utils import message
 
+def prep_snp_seqs(vcf_file, out_dir, seq_len, genome_fasta):
+    message('prep SNP sequences')
+
+    # Prepare hdf5 file
+    h5f = h5py.File('%s/model_in.h5'%out_dir, 'w')
+    dset = h5f.create_dataset('test_in', (1, 4, 1, seq_len), maxshape=(None, 4, 1, seq_len))
+
+    # Read through VCF
+    current_shape = 0 # start off with 0 (init to 1 bc got error otherwise)
+    with open(vcf_file, "r") as f:
+        for line in f:
+            # Get one hot coded sequence
+            snp = vcf.SNP(line)
+            seq_vecs, seqs, seq_headers = vcf.snps_seq1([snp], genome_fasta, seq_len)
+            seq_vecs = seq_vecs.reshape((seq_vecs.shape[0],4,1,seq_vecs.shape[1]/4))
+            # Add to hd5 file
+            dset.resize(current_shape+seq_vecs.shape[0], axis=0)
+            dset[current_shape:,...] = seq_vecs
+            current_shape = dset.shape[0]
+    h5f.close()
+
+
 def main():
     usage = "usage: %prog [options] <model_th> <vcf_file>"
     parser = OptionParser(usage)
@@ -22,6 +44,8 @@ def main():
     parser.add_option('-o', dest='out_dir', default='sad', help='Output directory for tables and plots [Default: %default]')
     parser.add_option('-l', dest='seq_len', type='int', default=600, help='Sequence length provided to the model [Default: %default]')
     parser.add_option('-t', dest='targets_file', default=None, help='File specifying target indexes and labels in table format')
+    parser.add_option('--only-generate-inputh5', dest='only_gen_inputh5', default=False, action='store_true', help='Do not run prediction step [Default: %default]')
+    parser.add_option('--only-run-pred', dest='only_run_pred', default=False, action='store_true', help='Input h5 file already generated. Only run prediction [Default: %default]')
 
     (options,args) = parser.parse_args()
     
@@ -37,25 +61,11 @@ def main():
     #################################################################
     # prep SNP sequences
     #################################################################
-    message('prep SNP sequences')
+    if not options.only_run_pred:
+        prep_snp_seqs(vcf_file, options.out_dir, options.seq_len, options.genome_fasta)
 
-    # Prepare hdf5 file
-    h5f = h5py.File('%s/model_in.h5'%options.out_dir, 'w')
-    dset = h5f.create_dataset('test_in', (1, 4, 1, options.seq_len), maxshape=(None, 4, 1, options.seq_len))
-
-    # Read through VCF
-    current_shape = 0 # start off with 0 (init to 1 bc got error otherwise)
-    with open(vcf_file, "r") as f:
-        for line in f:
-            # Get one hot coded sequence
-            snp = vcf.SNP(line, index_snp=options.index_snp, score=options.score)
-            seq_vecs, seqs, seq_headers = vcf.snps_seq1([snp], options.genome_fasta, options.seq_len)
-            seq_vecs = seq_vecs.reshape((seq_vecs.shape[0],4,1,seq_vecs.shape[1]/4))
-            # Add to hd5 file
-            dset.resize(current_shape+seq_vecs.shape[0], axis=0)
-            dset[current_shape:,...] = seq_vecs
-            current_shape = dset.shape[0]
-    h5f.close()
+    if options.only_gen_inputh5:
+        sys.exit(1)
 
     #################################################################
     # predict in Torch
